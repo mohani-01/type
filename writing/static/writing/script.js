b519b049403fb3b06a1ebf9e0607687cdc762f81
Nahom <nahomohani@gmail.com>
2023-10-01 20:55:07 +0300

    Conntect to API

--- writing/static/writing/script.js
+++ writing/static/writing/script.js
@@ -1,3 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("hello World");
-})
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+
+function startTyping(text) {
+
+    // let count = 0
+    // // const newLetter = document.getElementById("text").value[0]
+    // // console.log(newLetter);
+    // document.addEventListener('keyup', event => {
+    //     const letter = event.key;
+
+    //     if (letter === text[count]) {
+    //         console.log("correct", count)
+    //     }
+    //     count = count + 1;
+    //     console.log(count)
+
+
+    })
+}
+
+
+
+function getText() {
+
+    return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
+}
+
+
+async function renderText() {
+
+
+    const text = await getText();
+    document.getElementById("text").innerHTML = text;
+    startTyping(text)
+}
+
+
+renderText()
