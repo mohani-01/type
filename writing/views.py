fe801fd8bf9cee689676e3129bebfa2e6faa48b8
Nahom <nahomohani@gmail.com>
2023-10-15 07:01:54 +0300

    add some comment on views.py

--- writing/views.py
+++ writing/views.py
@@ -25,39 +25,38 @@ def index(request):
 def account(request):
 
     user = User.objects.get(username=request.user)
-
-    data_time =user.time.all()
+    # get all the user time and word score
+    data_time = user.time.all()
     data_word = user.word.all()
 
+    # combine both the time and words score on key=time, get only 25 of them
+    tables = sorted(chain(data_time, data_word), key=attrgetter('time'), reverse=True)[:25]
+
+
+    # get the sum of total time user spend on the game and total games played(in seconds)
     alltime = data_time.aggregate(Sum("time_frame"), Count("game_type"))
     allwords = data_word.aggregate(Sum("time_frame"), Count("game_type"))
 
     
-
-    tables = sorted(chain(data_time, data_word), key=attrgetter('time'), reverse=True)[:25]
-    print(tables)
-
-
-
- 
-
+    # get the sum of time_frame in alltime and allwords
     tt = time.gmtime(get_sum(alltime['time_frame__sum'], allwords['time_frame__sum']))
+
+    # then covert it into hour minute second format
     total_time = time.strftime("%H:%M:%S", tt)
+
+    # get the total number of games
     total_test = get_sum(alltime['game_type__count'], allwords['game_type__count'])
 
-
+    # get the best score of user in each time and words options
     times, words = get_data(request.user)
-
-
 
 
     return render(request, "writing/profile.html", {
         "times": times,
         "words": words,
-        "total_test":total_test,
         "total_time":total_time,
+        "total_test":total_test,
         "tables": tables
-        
     })
 
 
@@ -66,19 +65,24 @@ def account(request):
 
 @login_required(login_url='/login')
 def save_time(request):
-    
+
     if request.method == "POST":
-        print(request.user)
+
+        # get the data
         data = json.loads(request.body)
 
+        # check if all data's are avaiable
         if not data.get("wpm") or not  data.get("raw") or not data.get("accuracy") or not data.get("time") or not data.get("char"):
             return JsonResponse({"error":"One or more field is missing"}, status=400)
 
+        # test types to consider
         test_types = [15, 30, 60, 120]
 
+        # return incorect test type if the time isn't in the test types.
         if not int(data.get('time'))  in test_types:
-            print("Error", type(data.get('time')))
             return JsonResponse({"error": "Incorrect test type!"}, status=406)
+        
+        # save the test
         test = Test_Time(
             user = request.user,
             test_type = data.get("time"),
@@ -88,25 +92,28 @@ def save_time(request):
             raw_speed = data.get("raw"),
             character = data.get("char")
         )
-        test.save()
+
 
         return JsonResponse({"success":"Test is sucessfully saved."}, status=201)
  
-        
+@login_required(login_url='/login')    
 def save_word(request):
     if request.method == "POST":
-
+        # get the data
         data = json.loads(request.body)
-        
+
+        # check if all data's are avaiable
         if not data.get("wpm") or not  data.get("raw") or not data.get("accuracy") or not data.get("time") or not data.get("char") or not data.get("type"):
             return JsonResponse({"error":"One or more field is missing"}, status=400)
 
-
+        # test types to consider
         test_types = [10, 25, 50, 100]
 
+        # return incorrect test type if  data.get('type') is not in test_types 
         if not int(data.get('type'))  in test_types:
             return JsonResponse({"error": "Incorrect test type!"}, status=406)
        
+       # save the test
         test = Test_Word(
             user = request.user,
             test_type = data.get("type"),
@@ -117,11 +124,11 @@ def save_word(request):
             character = data.get("char")
         )
 
-        test.save()
+
 
         return JsonResponse({"success":"Test is sucessfully saved."}, status=201)
  
-    ...
+    
 
 
 def login_view(request):
@@ -186,6 +193,5 @@ def register(request):
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
 
-    # Via not POST ~ GET
     else:
         return render(request, "writing/login.html")
